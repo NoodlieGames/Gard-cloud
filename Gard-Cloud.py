242f1a0c96b309f01bb99a69e7329b0c5c113d47
WARDGOGG <161535243+WARDGOGG@users.noreply.github.com>
2024-05-17 14:23:40 +0300

    Add files via upload

---
+++ Gard-Cloud.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, send_from_directory, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+@app.route('/')
+def index():
+    uploaded_files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('upload.html', files=uploaded_files)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(url_for('index'))  # Redirect back to main page
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(url_for('index'))  # Redirect back to main page
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))  # Redirect back to main page
+
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
